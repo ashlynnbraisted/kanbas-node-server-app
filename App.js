a6200a8a2ef77e188690d5d64c555f2f94f2fc38
gaikwadsid <sidgaikwad853@gmail.com>
2024-04-21 22:52:39 -0400

    quiz branch

--- App.js
+++ App.js
@@ -26,7 +26,7 @@ const sessionOptions = {
       sameSite: "none",
       secure: true,
       //domain: process.env.HTTP_SERVER_DOMAIN,
-    };
+    }; 
   }
   app.use(session(sessionOptions));
 app.use(express.json());

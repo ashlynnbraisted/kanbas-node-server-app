2e791100a6934eb27738507ff1020e99f8e53c53
gaikwadsid <sidgaikwad853@gmail.com>
2024-04-22 03:08:28 -0400

    Finished QUestion Editor

--- src/Kanbas/question/dao.js
+++ src/Kanbas/question/dao.js
@@ -5,3 +5,5 @@ export const createQuestion = (question) => {
 }
 
 export const findAllQuestionsByQuizId = (quizId) => model.find({quizId: quizId});
+export const findQuestionById = (questionId) => model.findById(questionId);
+export const updateQuestion = (questionId, question) =>  model.updateOne({ _id: questionId }, { $set: question });
--- src/Kanbas/question/routes.js
+++ src/Kanbas/question/routes.js
@@ -6,11 +6,28 @@ export default function QuestionRoutes(app) {
     };
     const findAllQuestionsByQuizId = async (req, res) => {
         const questions = await dao.findAllQuestionsByQuizId(req.params.quizId);
-        console.log(questions);
         res.json(questions);
     };
 
+    const findQuestionById = async (req, res) => {
+        console.log('id', req.params.questionId);
+        const question = await dao.findQuestionById(req.params.questionId);
+        console.log('question', question);
+        res.json(question);
+    };
+
+    const updateQuestion = async (req, res) => {
+        const { questionId } = req.params;
+        const status = await dao.updateQuestion(questionId, req.body);
+        const currentQuestion = await dao.findQuestionById(questionId);
+        res.json(status);
+      };
+
     app.post("/api/questions", createQuestion);
     app.get("/api/questions/:quizId", findAllQuestionsByQuizId);
+    app.get("/api/questions/id/:questionId", findQuestionById);
+    app.put("/api/questions/:questionId", updateQuestion);
+
 
 }
+     
